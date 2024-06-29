29fb38203aeb4b7bd5e7a1e5aa6a4a57728b915a
Nokijoto <nokijoto@protonmail.com>
2024-06-29 14:41:43 +0200

    Added Base Dto and Entties

---
+++ ProjektCzlowiekKomputer/Project.CrossCutting/Common/CrudOperationResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.CrossCutting.Common
+{
+    public abstract class CrudOperationResult<TDto>
+    {
+        public CrudOperationResultStatus Status { get; set; }
+
+        public TDto? Result { get; set; }
+    }
+}
