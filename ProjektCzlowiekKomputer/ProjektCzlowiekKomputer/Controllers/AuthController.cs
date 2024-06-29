58c4194ba1199f1575bb8d46a4b2fff387904bdb
Nokijoto <nokijoto@protonmail.com>
2024-06-29 14:32:54 +0200

    Added Auth
    Simple Authorization and Authentication

---
+++ ProjektCzlowiekKomputer/ProjektCzlowiekKomputer/Controllers/AuthController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project.Data.Models;
+using ProjektCzlowiekKomputer.Interfaces;
+
+namespace ProjektCzlowiekKomputer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        private readonly ILogger<AuthController> _logger;
+
+        public AuthController(IAuthService authService, ILogger<AuthController> logger)
+        {
+            _authService = authService;
+            _logger = logger;
+        }
+
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest("Invalid payload");
+                var (status, message) = await _authService.Login(model);
+                if (status == 0)
+                    return BadRequest(message);
+                return Ok(message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register(RegistrationModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest("Invalid payload");
+                var (status, message) = await _authService.Registeration(model, UserRoles.User);
+                if (status == 0)
+                {
+                    return BadRequest(message);
+                }
+                return CreatedAtAction(nameof(Register), model);
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("RegisterAdmin")]
+        public async Task<IActionResult> RegisterAdmin(RegistrationModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest("Invalid payload");
+                var (status, message) = await _authService.Registeration(model, UserRoles.Admin);
+                if (status == 0)
+                {
+                    return BadRequest(message);
+                }
+                return CreatedAtAction(nameof(Register), model);
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
---
+++ ProjektCzlowiekKomputer/ProjektCzlowiekKomputer/Interfaces/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Project.Data.Models;
+
+namespace ProjektCzlowiekKomputer.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<(int, string)> Registeration(RegistrationModel model, string role);
+        Task<(int, string)> Login(LoginModel model);
+    }
+}
